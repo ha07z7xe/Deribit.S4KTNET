18cdc3487e742440f0ba574cd4f3303e3859c358
Hassan <hassan.ali.3821@outlook.com>
2019-06-02 11:43:24 +0000

    GetSubAccounts (not finished)

---
+++ Deribit.S4KTNET.Core/src/deribit/accountmanagement/requests/GetSubAccountsRequest.cs
@@ -0,0 +1,25 @@
+using Deribit.S4KTNET.Core.Mapping;
+using FluentValidation;
+
+namespace Deribit.S4KTNET.Core.AccountManagement
+{
+    // https://docs.deribit.com/v2/#private-get_subaccounts
+
+    public class GetSubAccountsRequest : RequestBase
+    {
+        public bool? with_portfolio { get; set; }
+
+        internal class Profile : AutoMapper.Profile
+        {
+            public Profile()
+            {
+                this.CreateMap<GetSubAccountsRequest, GetSubAccountsRequestDto>();
+            }
+        }
+    }
+
+    public class GetSubAccountsRequestDto
+    {
+        public bool? with_portfolio { get; set; }
+    }
+}
